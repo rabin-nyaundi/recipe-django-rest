f5b68c32a9370834228598e0b56b7e287d625b01
rabin-nyaundi <nyaundirabin@gmail.com>
2022-05-14 14:49:36 +0300

    update admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,6 @@
-from pyexpat import model
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, \
-    BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
 
 class UserManager(BaseUserManager):
